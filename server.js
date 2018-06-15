75e69595c6bbe7f677c5cc423fcc164533a68faa
Chad Pilker <chad.pilker@gmail.com>
2018-06-15 18:43:06 -0400

    initial commit, pending updated Readme and comments

---
+++ server.js
@@ -0,0 +1,24 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+//Set up the Express App
+var app = express();
+var PORT = 3000;
+app.use(express.static('app/public'))
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// Listener
+// ===========================================================
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+  });
+
